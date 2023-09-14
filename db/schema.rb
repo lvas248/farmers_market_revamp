7ce79623c0483901fcf4a5248e6c45669e5c2d50
Luis <lvas248@gmail.com>
2023-09-14 17:51:35 -0400

    creates order resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_14_214907) do
+ActiveRecord::Schema.define(version: 2023_09_14_215044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.boolean "open", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -34,4 +42,5 @@ ActiveRecord::Schema.define(version: 2023_09_14_214907) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "users"
 end
