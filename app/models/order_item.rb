b1c0c31a05ac6c3b665826dfc716795067a3f1b3
Luis <lvas248@gmail.com>
2023-09-14 23:11:33 -0400

    Backend: create validation in orderItem model that confirms that there is inventory availbel before adding it to the open order

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,14 @@ class OrderItem < ApplicationRecord
     belongs_to :product
     belongs_to :order
   
+    validate :confirm_inventory_available
+    
+    private
+
+    def confirm_inventory_available
+      unless self.order_qty <= self.product.qty_avail
+        errors.add(:order_qty, "Not enough inventory to fulfill")
+      end
+    end
+
   end
