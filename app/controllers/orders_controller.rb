c1ca47467a1777e13dc65a23add9d3e8cc4c296b
Luis <lvas248@gmail.com>
2023-09-18 14:13:35 -0400

    Backend: udpate order model and controller to account fro database restructure

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,63 +1,6 @@
 class OrdersController < ApplicationController
 
 
-    def add_to_cart
-
-        user = User.find_by(id: session[:user_id]) || Guest.find_by(id: session[:guest_id])
-
-
-        if user.present?
-            order = user.orders.find_or_create_by(open: true)
-            order_item = order.update_or_create_order_item(order_item_params)
-            render json: order_item, status: :created
-
-        else
-            new_guest = Guest.create!()
-            session[:guest_id] = new_guest.id
-            order = new_guest.get_cart
-            order_item = order.order_items.create!(order_item_params)
-            render json: order_item, status: :created
-        end
-
-
-    end
-
-    def remove_from_cart
-
-        user = User.find_by(id: session[:user_id]) || Guest.find_by(id: session[:guest_id])
-        if user.present?
-            order = user.get_cart
-            order.remove_order_item_by_id(params[:order_item_id])
-            head :no_content
-        else
-            render_not_found
-        end
-    end
-
-    def updateOrderItemQtyInCart
-
-        user = User.find_by(id: session[:user_id]) || Guest.find_by(id: session[:guest_id])
-       
-        if user.present?
-            order = user.orders.find_by(open:true)
-            order_item = order.order_items.find(params[:order_item_id])
-            order_item.update!(order_item_params)
-            render json: order_item, status: :created
-        else 
-            render_not_found
-        end
-
-    end 
-
-
-    def clear_cart
-        user = User.find_by(id: session[:user_id]) || Guest.find_by(id: session[:guest_id])
-        user.orders.find_by(open: true).order_items.each{ |i| i.destroy }
-        head :ok
-    end
-
-   
-
     def submit_order
         user = User.find_by(id: session[:user_id]) || Guest.find_by(id: session[:guest_id])
         user.orders.find_by(open: true)
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,47 +1,28 @@
 class Order < ApplicationRecord
     
-    belongs_to :imageable, polymorphic: true
-  
-    has_many :order_items, dependent: :destroy
-    has_many :products, through: :order_items
-  
-    def update_or_create_order_item(product_params)
-      order_item = self.order_items.find_by(product_id: product_params[:product_id])
-      order_item ? order_item.add_to_qty(product_params[:order_qty]) : ( 
-        order_item = self.order_items.create!(product_params) )
-      order_item
-    end
+  belongs_to :imageable, polymorphic: true
 
-    def remove_order_item_by_id(order_item_id)
-      self.order_items.find_by(id: order_item_id).destroy
-    end
+  has_many :order_items, as: :itemable, dependent: :destroy
 
-    def finalize_order
-      #update product qtys
-      self.order_items.each{ |o| o.fulfill_order_item }
-      #closes order
-      self.update!(open: false)
-    end
+  has_many :products, through: :order_items
 
-    def merge_with_order(guest_cart)
-      
-      # For each order_item in guest_cart, search user cart to if it has an order_item with the same product
-        guest_cart.order_items.each do |i|
-
-          order_item = self.order_items.find_by(product_id: i.product_id)
-      
-      # if user_cart has product already, update user_cart.order_item.order_qty => guest_cart qty + user_cart qty
-        
-        if order_item
-            order_item.update!(order_qty: order_item.order_qty + i.order_qty)
-          else
-            self.order_items.create!(product_id: i.product_id, order_qty: i.order_qty)
-          end
-        end
-
-      
-    end
-
-    
-  
+  def update_or_create_order_item(product_params)
+    order_item = self.order_items.find_by(product_id: product_params[:product_id])
+    order_item ? order_item.add_to_qty(product_params[:order_qty]) : ( 
+      order_item = self.order_items.create!(product_params) )
+    order_item
   end
+
+  def remove_order_item_by_id(order_item_id)
+    self.order_items.find_by(id: order_item_id).destroy
+  end
+
+  def finalize_order
+    #update product qtys
+    self.order_items.each{ |o| o.fulfill_order_item }
+    #closes order
+    self.update!(open: false)
+  end
+
+
+end
