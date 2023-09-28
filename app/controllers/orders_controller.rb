a91c938da71c0d12026c7292e2993f8b7186dbdb
Luis <lvas248@gmail.com>
2023-09-27 20:20:22 -0400

    Backend: refactor order controller actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,21 +7,16 @@ class OrdersController < ApplicationController
     end
 
     def submit_order
-    
-        user = User.find_by(id: session[:user_id]) || Guest.find_by(id: session[:guest_id])
-
-        new_order = user.orders.create!(order_params)
-        user.cart.clear_cart
-        
+        cart_owner = get_cart_owner
+        new_order = cart_owner.orders.create!(order_params)
+        cart_owner.cart.clear_cart
         render json: new_order, status: :created
-     
-
     end
 
     private
 
-    def get_user
-        user = User.find(session[:user_id])
+    def get_cart_owner
+        User.find_by(id: session[:user_id]) || Guest.find_by!(id: session[:guest_id])
     end
 
     def order_params
